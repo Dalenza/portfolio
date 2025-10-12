9122fdbda6e13eae5ae9629738a2ee6b8ee7c684
dalenza <dalydiiheb@gmail.com>
2025-10-12 20:07:13 +0100

    fix: grid layout set to 2 cols. tailwindcss doesn't dynamically generate utility classes, instead they are statically parsed

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -4,15 +4,9 @@ import { ProjectCard } from "./project-card";
 export function Projects() {
   return (
     <div className="grid grid-cols-1 gap-4 xl:grid-cols-6">
-      {DATA.projects.map((project, i) => {
-        const span: Record<number, number> = {
-          0: 3,
-          1: 3,
-          2: 3,
-          3: 3,
-        };
+      {DATA.projects.map((project) => {
         return (
-          <div key={project.title} className={`xl:col-span-${span[i]}`}>
+          <div key={project.title} className={`xl:col-span-3`}>
             <ProjectCard project={project} className="h-full" />
           </div>
         );
