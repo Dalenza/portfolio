957a93fbbcb8d967bc6448917958cfbfb8bf45cc
dalenza <dalydiiheb@gmail.com>
2025-10-13 10:17:37 +0100

    refactor:sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${soraFont.variable} font-sans [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-primary-foreground [&::-webkit-scrollbar-thumb]:bg-primary [&::-webkit-scrollbar-thumb]:hover:bg-primary/80 `}
+        className={`${soraFont.variable} font-sans [&::-webkit-scrollbar]:w-1 [&::-webkit-scrollbar-track]:bg-primary-foreground [&::-webkit-scrollbar-thumb]:bg-primary [&::-webkit-scrollbar-thumb]:hover:bg-primary/80 `}
       >
         {children}
       </body>
